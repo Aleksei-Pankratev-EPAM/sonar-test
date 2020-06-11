82e8860af8bba8b1340e8a23a4d3d8f5a75ee662
Aleksei <pankraty@gmail.com>
2020-06-11 17:24:42 +0400

    A sample to reproduce false positive alert

---
+++ SonarTest/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SonarTest
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleService service = null;
+
+            Console.WriteLine("Press the key");
+            var key = Console.ReadKey();
+
+            if (key.KeyChar == '1')
+                try
+                {
+                    service = Activator.CreateInstance(typeof(SampleService)) as SampleService;
+                }
+                catch
+                {
+                    //do nothing
+                }
+
+            if (service?.IsFeatureAvailable(FeatureFlag.Feature1) ?? false)
+            {
+                Console.WriteLine("I'm not dead");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class SampleService
+    {
+        public bool IsFeatureAvailable(FeatureFlag feature)
+        {
+            return feature == FeatureFlag.Feature1;
+        }
+    }
+
+    public enum FeatureFlag
+    {
+        Feature1,
+        Feature2
+    }
+}
